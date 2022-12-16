a9f7143618102d657c7e7a058f8d3ea915ec8565
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-12-16 20:16:35 +1100

    updtae

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function navBar() {
+  return (
+    <Navbar className="position-absolute top-0 start-50 translate-middle-x">
+      <Container>
+        <Navbar.Brand href="#home">Salt Integration</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Projects">Projects</Nav.Link>
+            <Nav.Link href="#Resume">Resume</Nav.Link>
+            <NavDropdown title="Links" id="basic-nav-dropdown">
+              <NavDropdown.Item href="https://github.com/AlexMastroianni">
+                Github
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://www.linkedin.com/in/alex-mastroianni/">
+                Linkedin
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default navBar;
